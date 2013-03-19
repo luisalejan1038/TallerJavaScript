8705ae1a0b89c068d698b78886ecb260a7503a90
unknown <luis.silva@VISUALSALE2.mind.com.co>
2013-03-19 12:37:01 -0500

    Lista funcion addClient

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 	var arrayToLoad = new Array(); 
-	
-	
-	function loadClients(array){
+		
+	function loadClients(array)
+	{
 		for(i = 0; i <= 50; i++){
 			var client = new Object(); 
 			client.id = i; 
@@ -14,4 +14,32 @@
 	}
 	
 	loadClients(arrayToLoad); 
-	console.log(arrayToLoad);
+//	console.log(arrayToLoad);
+	
+	var newClient = new Object(); 
+	newClient.id = "125"; 
+	newClient.name = "Cliente50"; 
+	newClient.email = "Cliente125@.com"; 
+	newClient.tel = "123456781";
+	newClient.descripcion = "Comprometido y serio";
+
+	function addClient (nClient, array)
+	{
+		var number = /\d{9}/; 
+		var email = /@\*(?:.com)/; 
+		var name = " ";
+		var okNumber = number.exec(nClient.tel);
+		var okEmail = email.exec(nClient.email); 
+		if (okNumber && (name != nClient.name) && okEmail)
+		{
+			array.push(nClient); 
+		}
+		else
+		{
+			window.alert("Verify number, telephone or email");
+		}		
+	}
+	
+	addClient(newClient, arrayToLoad); 
+//	console.log(arrayToLoad);
+	
